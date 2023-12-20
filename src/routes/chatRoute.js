00a7bd4cb58989e0ecbd270b6e48c0e9df4b3b07
mdamkjer <120249317+mdamkjer@users.noreply.github.com>
2023-12-20 12:47:49 +0000

    chatRoute

--- src/routes/chatRoute.js
+++ src/routes/chatRoute.js
@@ -10,38 +10,45 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get('/user-favorites', async (req, res) => {
-  try {
-    // Assuming the preferences are sent in the request body
-    const { username } = req.body;
+// Map to store online users
+let onlineUsers = {};
 
-    // SQL query to fetch user favorites from the database
+// Route to check matching users
+router.post('/user-favorites', async (req, res) => {
+ const userFavorites = req.body;
+
+ try {
+    // SQL query to fetch online users with matching preferences
     const query = `
-    SELECT u.Juice, u.Coffee, u.Sandwich
+    SELECT DISTINCT u.UserName
     FROM Users u
-    WHERE u.UserName = @username;
-    `;
+    WHERE u.Juice = @juice AND u.Coffee = @coffee AND u.Sandwich = @sandwich
+    AND u.UserName IN (${Object.values(onlineUsers).map((username) => `'${username}'`).join(',')});
+ `;
 
     // Execute the SQL query
-    const userFavorites = await executeSQL(query, [{ name: 'username', type: TYPES.VarChar, value: username }]);
+    const onlineMatchingUsers = await executeSQL(query, [
+      { name: 'juice', type: TYPES.VarChar, value: userFavorites.juice },
+      { name: 'coffee', type: TYPES.VarChar, value: userFavorites.coffee },
+      { name: 'sandwich', type: TYPES.VarChar, value: userFavorites.sandwich },
+      ...Object.values(onlineUsers).map((username) => ({ name: 'username', type: TYPES.VarChar, value: username })),
+    ]);
 
-    res.json(userFavorites);
-  } catch (error) {
-    console.error('Error fetching user favorites:', error);
+    // Return the list of online users with matching preferences
+    res.json(onlineMatchingUsers);
+ } catch (error) {
+    console.error('Error checking matching users:', error);
     res.status(500).json({ error: 'Internal Server Error' });
-  }
+ }
 });
-
-// Socket.IO event for tracking online users
-const onlineUsers = {};
 
 // Function to set up Socket.IO connection and pass the socket to the routes
 const setupSocketConnection = (io) => {
-  io.of('/chat').on('connection', (socket) => {
+ io.of('/chat').on('connection', (socket) => {
     console.log(`User ${socket.id} connected`);
 
     // Socket.IO event for user joining the chat
-      socket.on('user joined', (username) => {
+    socket.on('user joined', (username) => {
       console.log(`User ${username} joined the chat`);
       onlineUsers[socket.id] = username;
     });
@@ -52,37 +59,9 @@ const setupSocketConnection = (io) => {
       delete onlineUsers[socket.id];
     });
 
-    // Socket.IO event for checking matching users
-    socket.on('check_matching_users', async (preferences) => {
-      const { juice, coffee, sandwich } = preferences;
-
-      try {
-        // SQL query to fetch online users with matching preferences
-        const query = `
-        SELECT DISTINCT u.UserName
-        FROM Users u
-        WHERE u.Juice = @juice AND u.Coffee = @coffee AND u.Sandwich = @sandwich
-        AND u.UserName IN (${Object.values(onlineUsers).map((username) => `'${username}'`).join(',')});
-      `;
-
-        // Execute the SQL query
-        const onlineMatchingUsers = await executeSQL(query, [
-          { name: 'juice', type: TYPES.VarChar, value: juice },
-          { name: 'coffee', type: TYPES.VarChar, value: coffee },
-          { name: 'sandwich', type: TYPES.VarChar, value: sandwich },
-          ...Object.values(onlineUsers).map((username) => ({ name: 'username', type: TYPES.VarChar, value: username })),
-        ]);
-
-        // Emit the list of online users with matching preferences
-        socket.emit('matching_users', onlineMatchingUsers);
-      } catch (error) {
-        console.error('Error checking matching users:', error);
-      }
-    });
-
-    // // Include your chatRoute and pass the socket
-    // chatRoute(io, socket);
-  });
+    // Include your chatRoute and pass the socket
+    chatRoute(io, socket);
+ });
 };
 
 module.exports = { router, setupSocketConnection };
