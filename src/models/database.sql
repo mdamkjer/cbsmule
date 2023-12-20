47132165f30c90e461fe947fbff55c7a40fd749d
mdamkjer <120249317+mdamkjer@users.noreply.github.com>
2023-12-20 21:02:06 +0100

    Update database.sql

--- src/models/database.sql
+++ src/models/database.sql
@@ -8,19 +8,3 @@ CREATE TABLE Users (
     Coffee VARCHAR(50),
     Sandwich VARCHAR(50)
 );
-
-CREATE TABLE Favorites (
-    FavoritesID INT IDENTITY(1,1) PRIMARY KEY,
-    Juice VARCHAR(50),
-    Coffee VARCHAR(50),
-    Sandwich VARCHAR(50)
-);
-
-CREATE TABLE UserFavorites (
-    UserFavoritesID INT IDENTITY(1,1) PRIMARY KEY,
-    UserID INT,
-    FavoritesID INT,
-    FOREIGN KEY (UserID) REFERENCES Users(UserID),
-    FOREIGN KEY (FavoritesID) REFERENCES Favorites(FavoritesID)
-);
-
